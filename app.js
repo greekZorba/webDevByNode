30dae11022849fdb0c22a51b87be9c3270943960
jinhak Kim <smiling.jinhak@gmail.com>
2018-10-06 15:35:56 +0900

    POST 요청처리
    데이터 통신을 위해 body-parser 필요
    
    설치 명령어(for mac):
    npm install body-parser --save

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser');
 
 app.listen(3000, function() {
 	console.log("start! express server on port 3000");
 });
 
 app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
 
+// url routing
 app.get('/', function(req, res){
 	
 	res.sendFile(__dirname+"/public/main.html");
@@ -15,4 +19,9 @@ app.get('/', function(req, res){
 app.get('/main', function(req, res){
 	
 	res.sendFile(__dirname+"/public/main.html");
+});
+ 
+app.post('/email_post', function(req, res){
+	console.log(req.body.email);
+	res.send('<h1> what did you type your email address? </h1>'+req.body.email);
 });
