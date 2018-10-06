11964e6d85f137c9cbe822452c2f21e6d9774657
jinhak Kim <smiling.jinhak@gmail.com>
2018-10-06 13:27:12 +0900

    URL Routing 처리 + static 디렉토리 설정

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var express = require('express')
 var app = express()
+
 app.listen(3000, function() {
 	console.log("start! express server on port 3000");
-	console.log("what???")
+});
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res){
+	
+	res.sendFile(__dirname+"/public/main.html");
+});
+
+app.get('/main', function(req, res){
+	
+	res.sendFile(__dirname+"/public/main.html");
 });
