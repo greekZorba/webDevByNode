f70d1fe008b62834bc5b04d36912f97a567b468c
jinhak Kim <smiling.jinhak@gmail.com>
2018-10-29 23:35:29 +0900

    DB에 데이터 추가 두번째 커밋
    insert 해보기

--- router/join/index.js
+++ router/join/index.js
@@ -20,5 +20,19 @@ router.get('/', function(req, res){
 	console.log('join index connect success');
 	res.sendFile(path.join(__dirname, "../../public/join.html"));
 });
+
+router.post('/', function(req, res){
+	var body = req.body;
+	var email = body.email;
+	var name = body.name;
+	var password = body.password;
+	
+	var query = connection.query('insert into node_inflearn_user(user_name, user_email, user_password) values("'+name+'", "'+email+'","'+password+'")', function(err,rows){
+		if(err){ throw err;}	
+		console.log('db insert success')
+	})
+	
+	
+})
  
 module.exports = router;
