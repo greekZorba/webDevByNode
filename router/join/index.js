b9f20eae18446145a99624c84d5c1f3e8e722084
jinhak Kim <smiling.jinhak@gmail.com>
2018-11-21 23:31:54 +0900

    local-strategy 콜백 적용

--- router/join/index.js
+++ router/join/index.js
@@ -19,16 +19,32 @@ connection.connect();
 
 // Router
 router.get('/', function(req, res){
-	console.log('join index connect success');
-	res.render('join.ejs', {message:'임시'});
+	var msg;
+	var errMsg = req.flash('error');
+	if(errMsg) msg = errMsg;
+	res.render('join.ejs', {'message' : msg});
 });
+
+// passport.serializeUser
 
 passport.use('local-join', new LocalStrategy({
 	usernameField:'email',
 	passwordField:'password',
 	passReqToCallback: true
 	}, function(req, email, password, done){
-		console.log('local-join callback called')
+		var query = connection.query('select * from node_inflearn_user where user_email = ?', [email], function(err,rows){
+			if(err) return done(err);
+
+			if(rows.length){
+				return done(null, false, {message : 'you already have email!'});
+			}else{
+				var sql = {'user_email':email, 'user_password':password};
+				var query = connection.query('insert into node_inflearn_user set ?', sql, function(err,rows){
+					if(err) throw err;
+					return done(null, {'email': email, 'id':rows.insertId})
+				})
+			}
+		})
 	}
 ));
 
